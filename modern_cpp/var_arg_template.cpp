9e78d0dbfa5fd20d8416f48069948792c681bdc3
liuyanan <>
2021-09-09 10:33:40 +0800

    添加了c++11 可变参数模板的用例

---
+++ modern_cpp/var_arg_template.cpp
@@ -0,0 +1,69 @@
+/*
+ * var_arg_template.cpp
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: liuyanan
+ */
+
+#include <iostream>
+#include <tuple>
+#include <vector>
+
+
+using namespace std;
+
+
+template <class... T>
+void arg_count(T... args) {
+	// 获取参数包的个数
+	cout << "get arg count: " << sizeof...(args) << endl;
+}
+
+
+/* ************************ 参数展开 (递归)	************************ */
+
+/*
+  关于参数展开的的递归流程：
+  	  当遇到第一个不含参数包的的函数时，就认为是终止函数
+ */
+
+void unfold_args()
+{
+	// 终止函数
+	cout << "arg-pack end" << endl;
+}
+
+template <typename T>
+void unfold_args(T head)
+{
+	// 终止函数
+	cout << "cur arg-pack head: " << head << endl;
+}
+
+template <typename T, typename X>
+void unfold_args(T head, X next)
+{
+	// 终止函数
+	cout << "cur arg-pack head: " << head << ", next: " << next << endl;
+}
+
+template <typename T, class ...Args>
+void unfold_args(T head, Args... rest)
+{
+	cout << "cur arg-pack head: " << head << endl;
+	unfold_args(rest...);
+}
+
+
+int main(int argc, char **argv) {
+
+	arg_count("param1", 2, 3.0, 4, 5);
+
+	cout << endl;
+
+	unfold_args("param1", 2, 3.0, 4, 5);
+
+	cout << endl;
+
+	return 0;
+}
--- tricks/class_derive_1.cpp
+++ tricks/class_derive_1.cpp
@@ -27,6 +27,11 @@ public:
 	virtual void doSomething() {
 		cout << "Base do something" << endl;
 	}
+
+	string foo() {
+		cout << "Base foo" << endl;
+		return "Base foo";
+	}
 };
 
 
@@ -68,34 +73,51 @@ private:
 
 int main()
 {
-	cout << "\n------------------------------ delete Derived pointer --------------------------------\n " << endl;
-	Derived *p_1 = new Derived();
-	p_1->doSomething();
-	delete p_1;
+	{
+		cout << "\n------------------------------ delete Derived pointer --------------------------------\n " << endl;
+		Derived *p_1 = new Derived();
+		p_1->doSomething();
+		delete p_1;
+	}
 
-	cout << "\n------------------------------ delete Base pointer ----------------------------------\n " << endl;
+	{
+		cout << "\n------------------------------ delete Base pointer ----------------------------------\n " << endl;
 
-	// 加不加 virtual输出的值是不一样的
-	// 看对象是哪个，就是调用哪个的方法
-	Base *p_2 = new Derived();
-	p_2->doSomething();
-	delete p_2;
+		// 加不加 virtual输出的值是不一样的
+		// 看对象是哪个，就是调用哪个的方法
+		Base *p_2 = new Derived();
+		p_2->doSomething();
+		delete p_2;
+	}
 
-	cout << "\n------------------------------ delete array pointer ----------------------------------\n " << endl;
+	{
+		cout << "\n------------------------------ delete array pointer ----------------------------------\n " << endl;
 
-	Derived* p_derived_array  = new Derived[3];
-	// delete p_derived_array;	// 如果用 delete 去释放一个数组，不仅仅是调用几次析构的问题，而是程序会直接崩溃
+		Derived* p_derived_array  = new Derived[3];
+		// delete p_derived_array;	// 如果用 delete 去释放一个数组，不仅仅是调用几次析构的问题，而是程序会直接崩溃
+	}
 
-	cout << "\n------------------------------ get member func address ----------------------------------\n " << endl;
+	{
+		cout << "\n------------------------------ get member func address ----------------------------------\n " << endl;
 
-	Derived d;
-	typedef string (Derived::*fun)();
-	fun f1 = &Derived::foo;
+		Derived d;
+		typedef string (Derived::*fun)();
+		fun f1 = &Derived::foo;
 
-	// ???为啥都是1
-	cout << "nonstatic member function address dummy:" << &Derived::dummy << ", foo:"  << &Derived::foo << endl;
+		// ???为啥都是1
+		cout << "nonstatic member function address dummy:" << &Derived::dummy << ", foo:"  << &Derived::foo << endl;
 
-	cout << "nonstatic member function called by fun pointer:" << (d.*f1)() << endl;
+		cout << "nonstatic member function called by fun pointer:" << (d.*f1)() << endl;
+	}
+
+
+	{
+		cout << "\n------------------------------ call non virtual function  start ----------------------------------\n " << endl;
+
+		Base *p_3 = new Derived();
+		p_3->foo();
+		delete p_3;
+	}
 
 	return 0;
 }
